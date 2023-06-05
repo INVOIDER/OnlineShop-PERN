f83f94d022eb19f25f6ca61ed9d583636f304a6e
INVOIDER <5261323@gmail.com>
2023-06-05 04:09:34 +0300

    Fixed db connection
    Changed bd connection type

--- server/db.js
+++ server/db.js
@@ -1,10 +1,9 @@
 const Pool = require('pg').Pool //Реализация подключения к бд с помощью pool
 require('dotenv').config() //Подключаем библиотеку переменных окружения
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    connectionString: process.env.CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false // Необязательно, если вы используете самоподписанный сертификат
+    }
 })
 module.exports = {pool}
